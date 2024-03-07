8cb02000fcdfc53e8e648ae646b5c1e924a8f4a9
Kasturirangan Iyengar <87088672+ikasturirangan@users.noreply.github.com>
2024-03-07 20:27:47 +0530

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Makeability Lab Handbook</span>,
+  project: {
+    link: 'https://twitter.com/makeabilitylab',
+  },
+  chat: {
+    link: 'https://makeabilitylab.cs.washington.edu/',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Makeability Lab Handbook - University of Washington ',
+  },
+}
+
+export default config
